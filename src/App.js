9425b9ec143216c338302b77e0ba3a09667e2690
Alain Perkaz <alain.perkaz@fortnox.se>
2022-11-07 11:41:06 +0100

    fix: huge new feature, breaks app
    BREAKING CHANGE: breaking changes!

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there, breaking change</p>
+        <p>Hi there, huge breaking change</p>
         <a
           className="App-link"
           href="https://reactjs.org"
